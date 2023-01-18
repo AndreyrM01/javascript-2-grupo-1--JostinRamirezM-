18bfbe42fd12cc711422f0075fd52e9a602300bc
Jostin <rjostin954@gmail.com>
2023-01-17 18:24:38 -0600

    mi prueba

--- main.js
+++ main.js
@@ -4,28 +4,28 @@
 
 // 1. Respuesta pregunta 1
 
-// Colocar la respuesta acá
+// Colocar la respuesta acá: let, var , const 
 
 // ====================================================================================================
 
 
 // 2. Respuesta pregunta 2
 
-// Colocar la respuesta acá
+// Colocar la respuesta acá: .push 
 
 // ====================================================================================================
 
 
 // 3. Respuesta pregunta 3
 
-// Colocar la respuesta acá
+// Colocar la respuesta acá: agregaria los elementos al array.
 
 // ====================================================================================================
 
 
 // 4. Respuesta pregunta 4
 
-// Colocar la respuesta acá
+// Colocar la respuesta acá: lo que esta haciendo es medir si != el valor es distinto y !== si es de un tipo distinto 
 
 // ====================================================================================================
 
@@ -62,7 +62,7 @@
 
 // 1. Respuesta pregunta 1
 
-// Colocar el código acá
+// Colocar el código acá: se que para distinguir entre par e impar lo qeu necesito es hacer un numero +1 y dividirlo entre 2 y lo demas ni idea.
 
 // ====================================================================================================
 
